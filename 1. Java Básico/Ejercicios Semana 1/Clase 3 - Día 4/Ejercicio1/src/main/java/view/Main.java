c6a8b6b6de81fe9e3954db8229772d629ac17d1a
Aaron Fernández <AaronMFM97@hotmail.com>
2024-02-21 19:28:41 +0100

    UPDATE Ejercicio1

--- 1. Java Básico/Ejercicios Semana 1/Clase 3 - Día 4/Ejercicio1/src/main/java/view/Main.java
+++ 1. Java Básico/Ejercicios Semana 1/Clase 3 - Día 4/Ejercicio1/src/main/java/view/Main.java
@@ -10,21 +10,21 @@ public class Main {
     //! ¿Se obtiene algún valor? De ser así… ¿Qué valor se obtiene?
     public static void main(String[] args) {
         //! a) Crea 3 objetos de la clase Electrodomestico utilizando parámetros.
-        Electrodomestico elec1 = new Electrodomestico("Cod1", "Marca1", "Modelo1", "A", "Rojo");
-        Electrodomestico elec2 = new Electrodomestico("Cod2", "Marca2", "Modelo2", "E", "Azul");
-        Electrodomestico elec3 = new Electrodomestico("Cod3", "Marca3", "Modelo3", "C", "Blanco");
+        Electrodomestico appliance1 = new Electrodomestico("Cod1", "Marca1", "Modelo1", "A", "Rojo");
+        Electrodomestico appliance2 = new Electrodomestico("Cod2", "Marca2", "Modelo2", "E", "Azul");
+        Electrodomestico appliance3 = new Electrodomestico("Cod3", "Marca3", "Modelo3", "C", "Blanco");
         //! b) Crea 1 objeto de la clase Electrodomestico sin parámetros.
-        Electrodomestico elecSinParam = new Electrodomestico();
+        Electrodomestico noParamAppliance = new Electrodomestico();
 
         //! c) Muestra por pantalla la marca, modelo y consumo energético de los 3 creados con parámetros.
         System.out.printf(
                 "Creados con parámetros:\n%s, %s - %s\n%s, %s - %s\n%s, %s - %s\n",
-                elec1.getMarca(), elec1.getModelo(), elec1.getConsumo(),
-                elec2.getMarca(), elec2.getModelo(), elec2.getConsumo(),
-                elec3.getMarca(), elec3.getModelo(), elec3.getConsumo()
+                appliance1.getMarca(), appliance1.getModelo(), appliance1.getConsumo(),
+                appliance2.getMarca(), appliance2.getModelo(), appliance2.getConsumo(),
+                appliance3.getMarca(), appliance3.getModelo(), appliance3.getConsumo()
         );
         //! d) Intenta obtener la marca del electrodoméstico creado sin parámetros.
-        System.out.println(elecSinParam.getMarca());
+        System.out.println(noParamAppliance.getMarca());
         //! ¿Se obtiene algún valor? De ser así… ¿Qué valor se obtiene?
         //? Se obtiene una representación de valor nulo al ser cadena. En caso de ser tipo de dato sería excepción.
 
